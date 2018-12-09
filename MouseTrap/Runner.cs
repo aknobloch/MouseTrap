89f7b7b17a158e697ceb2b197749d7921f00fe87
aknobloch <aarondevelops@gmail.com>
2018-12-09 12:18:02 -0500

    initial files

---
+++ MouseTrap/Runner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MouseTrap
+{
+    class Runner
+    {
+        [DllImport("user32")]
+        public static extern int SetCursorPos(int x, int y);
+
+        static void Main(string[] args)
+        {
+            SetCursorPos(Int32.MaxValue, Int32.MaxValue);
+        }
+    }
+}
